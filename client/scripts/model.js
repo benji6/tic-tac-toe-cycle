e3b0bc3f122729138bba8fd21cb99c6be644c109
benji6 <benhall22@hotmail.co.uk>
2015-08-29 09:23:25 +0100

    diagonals

--- client/scripts/model.js
+++ client/scripts/model.js
@@ -1,25 +1,24 @@
 import {
-  always, all, any, compose, empty, equals, filter, flatten, flip,
-  ifElse, length, map, nth, or, range, repeat,
+  always, all, any, compose, empty, equals, filter, flatten,
+  flip, ifElse, length, map, nth, or, range,
 } from 'ramda';
 import {mapIndexed} from './tools';
 
 const sideLength = 3;
-const seedRow = repeat(0, sideLength);
+const seedRange = range(0, sideLength);
 const codeToCellValue = flip(nth)([, 'O', 'X']);
 const filterLength = compose(length, filter);
 const isThreeInARow = list => or(all(equals(1))(list), all(equals(2))(list));
-const isVictory = board => any(isThreeInARow,
-                               [...board,
-                                ...map(x => map(nth(x),
-                                                board),
-                                       range(0, sideLength)),
-                                ...[]]);
+const allLines = board => [...board,
+                           ...map(x => map(nth(x), board), seedRange),
+                           map(x => board[x][x], seedRange),
+                           map(x => board[x][sideLength - 1 - x], seedRange)];
+const isVictory = compose(any(isThreeInARow), allLines);
 const computeBoardViewModel = map(map(codeToCellValue));
 const computeMessageViewModel = ifElse(isVictory, always('someone wins'), empty);
 
 export default ({move$}) => move$
-  .startWith([for (i of seedRow) [for (j of seedRow) 0]]) // eslint-disable-line no-unused-vars
+  .startWith([for (i of seedRange) [for (j of seedRange) 0]]) // eslint-disable-line no-unused-vars
   .scan((acc, [row, col]) => mapIndexed((x, i) => mapIndexed((y, j) => i === Number(row) && j === Number(col) && acc[row][col] === 0 ?
     filterLength(equals(1), flatten(acc)) > filterLength(equals(2), flatten(acc)) ?
       2 :
