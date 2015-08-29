f6f5b56d0d65f7b9a5d203af3b9296b09d284a34
benji6 <benhall22@hotmail.co.uk>
2015-08-29 08:48:01 +0100

    some tidying

--- client/scripts/model.js
+++ client/scripts/model.js
@@ -1,27 +1,26 @@
 import {
-  all, any, compose, concat, equals, filter, flatten, flip, length, map, nth, or, range
+  always, all, any, compose, empty, equals, F, filter, flatten, flip,
+  inc, ifElse, length, map, nth, or, range, repeat, unfold
 } from 'ramda';
 import {mapIndexed} from './tools';
 
 const sideLength = 3;
 const codeToCellValue = flip(nth)([, 'O', 'X']);
 const filterLength = compose(length, filter);
-
 const isThreeInARow = list => or(all(equals(1))(list), all(equals(2))(list));
-
+const isVictory = board => any(isThreeInARow,
+                               [...board,
+                                ...map(x => map(nth(x),
+                                                board),
+                                       range(0, sideLength)),
+                                ...[]]);
 const computeBoardViewModel = map(map(codeToCellValue));
-const computeMessageViewModel = board => any(isThreeInARow,
-                                             concat(board,
-                                                    map(x => map(nth(x),
-                                                                 board),
-                                                        range(0, sideLength)))) ?
-                                                          'someone wins' :
-                                                          '';
+const computeMessageViewModel = ifElse(isVictory, always('someone wins'), empty);
 
 export default ({move$}) => move$
-  .startWith(Array.from({length: sideLength}, () => Array.from({length: sideLength}, () => 0)))
+  .startWith(unfold(ifElse(equals(sideLength), F, n => [repeat(0, sideLength), inc(n)]), 0))
   .scan((acc, [row, col]) => mapIndexed((x, i) => mapIndexed((y, j) => i === Number(row) && j === Number(col) && acc[row][col] === 0 ?
-    filterLength(equals(1))(flatten(acc)) > filterLength(equals(2))(flatten(acc)) ?
+    filterLength(equals(1), flatten(acc)) > filterLength(equals(2), flatten(acc)) ?
       2 :
       1
     : y, x), acc))
