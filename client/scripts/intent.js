a4e96ee784801285609f390257bb14bf84153a65
benji6 <benhall22@hotmail.co.uk>
2015-10-26 14:11:49 +0000

    update deps

--- client/scripts/intent.js
+++ client/scripts/intent.js
@@ -1,3 +1,3 @@
 import {map} from 'ramda';
 export default DOM => ({move$: map(e => e.target.id,
-                                   DOM.get('.board .cell', 'click'))});
+                                   DOM.select('.board .cell').events('click'))});
